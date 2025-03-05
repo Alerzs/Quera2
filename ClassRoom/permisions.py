8090ac77e6152c1d741f26712b10906cb4582708
Alireza <alireza.shams2106@gmail.com>
2025-03-05 17:26:33 +0330

    refactor: :recycle: new permisions were added

--- ClassRoom/permisions.py
+++ ClassRoom/permisions.py
@@ -5,9 +5,11 @@ class IsClassOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.is_owner(request.user)
     
+
 class IsClassTeacherOrMentor(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.is_teacher_or_mentor(request.user)
+    
     
 class IsClassMember(BasePermission):
     def has_object_permission(self, request, view, obj):
